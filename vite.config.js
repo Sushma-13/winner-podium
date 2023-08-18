b45b857947ddf77e787f30b5b851a12c730b8e2f
Sushma Goswami <goswamisushma30@gmail.com>
2023-08-18 22:21:28 +0530

    Update build config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { resolve } from 'node:path'
-import * as packageJson from './package.json'
+import { resolve } from "node:path";
+import * as packageJson from "./package.json";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve('src', 'components/index.js'),
-      name: 'WinnersPodiumLibrary',
-      formats: ['es', 'umd'],
+      entry: resolve("src", "components/index.js"),
+      name: "WinnersPodiumLibrary",
+      formats: ["es", "umd"],
       fileName: (format) => `winners-podium-library.${format}.js`,
     },
     rollupOptions: {
@@ -16,4 +16,5 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  publicDir: "",
 });
